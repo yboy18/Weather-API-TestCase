03632692911c168edd24b250d3bd304747d9d3b7
Boris <yboy@live.hk>
2016-08-30 04:59:23 +0000

    v.3

--- test-case.3/app.js
+++ test-case.3/app.js
@@ -26,7 +26,7 @@ if ('development' == app.get('env')) {
   app.use(express.errorHandler());
 }
 
-app.get('/', function(){res.render('message', {title:'Conventry Node.js API - Test Case Data: 2016-08-21'});});
+app.get('/', function(req, res){res.render('message', {title:'Conventry Node.js API - Test Case Data: 2016-08-21'});});
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
